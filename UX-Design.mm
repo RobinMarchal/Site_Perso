64b717289c035b19bb2bb151d4119db952de656e
RobinR2 <rmarchal@r2.fr>
2018-10-03 16:20:21 +0200

    Veille techno UX UI

---
+++ UX-Design.mm
@@ -0,0 +1,326 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="      UX / UI Design" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1573594237" CREATED="1462710693327" MODIFIED="1538575032327" TEXT_ALIGN="CENTER" MAX_WIDTH="260.0 pt" MIN_WIDTH="182.0 pt">
+<font SIZE="16"/>
+<hook NAME="MapStyle" background="#f7f7f7">
+    <properties show_icon_for_attributes="true" fit_to_viewport="false" show_note_icons="true" edgeColorConfiguration="#808080ff,#a3a3ffff,#ff77b3ff,#ff9933ff,#9999ffff,#968900ff,#cc00ccff,#ffbf00ff,#00ff99ff,#0099ffff,#996600ff,#000000ff,#cc0066ff,#33ff00ff,#ff9999ff,#0000ccff,#cccc00ff,#0099ccff,#006600ff,#ff00ccff,#00cc00ff,#0066ccff,#00ffffff"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt" TEXT_SHORTENED="true">
+<font SIZE="24"/>
+<richcontent TYPE="DETAILS" LOCALIZED_HTML="styles_background_html"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<font SIZE="9"/>
+<stylenode LOCALIZED_TEXT="default" COLOR="#000000" STYLE="bubble" SHAPE_VERTICAL_MARGIN="0.0 pt" TEXT_ALIGN="CENTER" MAX_WIDTH="120.0 pt" MIN_WIDTH="120.0 pt">
+<font NAME="Arial" SIZE="9" BOLD="false" ITALIC="false"/>
+<edge STYLE="bezier" WIDTH="1"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes" COLOR="#000000" BACKGROUND_COLOR="#cccccc" MAX_WIDTH="260.0 pt" MIN_WIDTH="260.0 pt" VGAP_QUANTITY="5.0 pt">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<font SIZE="9"/>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+<edge COLOR="#0000cc"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<font SIZE="9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" STYLE="fork" UNIFORM_SHAPE="true" TEXT_ALIGN="CENTER" MAX_WIDTH="120.0 pt" MIN_WIDTH="100.0 pt" VGAP_QUANTITY="10.0 pt">
+<font SIZE="16"/>
+<edge STYLE="bezier" WIDTH="3"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" NUMBERED="true" MIN_WIDTH="100.0 pt" VGAP_QUANTITY="5.0 pt">
+<font SIZE="11" BOLD="true"/>
+<edge COLOR="#a3a3ff" WIDTH="1"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" MAX_WIDTH="200.0 pt" MIN_WIDTH="20.0 pt" VGAP_QUANTITY="5.0 pt" TEXT_ALIGN="LEFT">
+<edge COLOR="#ff77b3" WIDTH="1"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" MAX_WIDTH="260.0 pt" MIN_WIDTH="20.0 pt" TEXT_ALIGN="LEFT" VGAP_QUANTITY="5.0 pt">
+<edge COLOR="#ff9933" WIDTH="1"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" MAX_WIDTH="260.0 pt" MIN_WIDTH="20.0 pt" TEXT_ALIGN="LEFT" VGAP_QUANTITY="5.0 pt">
+<edge COLOR="#9999ff" WIDTH="1"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5">
+<edge COLOR="#968900"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6">
+<edge COLOR="#cc00cc"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7">
+<edge COLOR="#ffbf00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8">
+<edge COLOR="#00ff99"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9">
+<edge COLOR="#0099ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10">
+<edge COLOR="#996600"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11">
+<edge COLOR="#000000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,12">
+<edge COLOR="#cc0066"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,13">
+<edge COLOR="#33ff00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,14">
+<edge COLOR="#ff9999"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,15">
+<edge COLOR="#0000cc"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,16">
+<edge COLOR="#cccc00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,17">
+<edge COLOR="#0099cc"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,18">
+<edge COLOR="#006600"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,19">
+<edge COLOR="#ff00cc"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,20">
+<edge COLOR="#00cc00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,21">
+<edge COLOR="#0066cc"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,22">
+<edge COLOR="#00ffff"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="0" RULE="FOR_BRANCHES"/>
+<edge COLOR="#000000"/>
+<node TEXT="Description" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" POSITION="left" ID="ID_1622075986" CREATED="1462726803381" MODIFIED="1537370111185" BACKGROUND_COLOR="#c1c1ff" VGAP_QUANTITY="5.0 pt">
+<node TEXT="D&#xe9;finir l&apos;UX" ID="ID_1173160912" CREATED="1537370816845" MODIFIED="1537372253690">
+<font SIZE="12"/>
+<node TEXT="Ensemble de normes et de pratiques dont l&apos;objectif est de concevoir un produit, mais aussi l&apos;exp&#xe9;rience de son utilisation." LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_1868374267" CREATED="1537370092567" MODIFIED="1537372230121" HGAP_QUANTITY="31.249999485909953 pt" VSHIFT_QUANTITY="-31.499999061226873 pt">
+<font SIZE="12"/>
+</node>
+<node TEXT="Ergonomie du produit et de son usage &amp; chercher le besoin de l&apos;utilisateur (ses attentes)" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_1141880828" CREATED="1537370092567" MODIFIED="1537372246922" HGAP_QUANTITY="31.249999485909953 pt" VSHIFT_QUANTITY="-31.499999061226873 pt">
+<font SIZE="12"/>
+</node>
+<node TEXT="R&#xe9;ponde pr&#xe9;cisement aux besoins et attentes de l&apos;utilisateur pour une exp&#xe9;rience unique." LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_1900167122" CREATED="1537370092567" MODIFIED="1537372237693" HGAP_QUANTITY="31.249999485909953 pt" VSHIFT_QUANTITY="-31.499999061226873 pt">
+<font SIZE="12"/>
+</node>
+<node TEXT="Dimension psychologique importante, toucher le coeur du besoin chez l&apos;utilisateur." LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_1018458048" CREATED="1537370521965" MODIFIED="1537372250768">
+<font SIZE="12"/>
+</node>
+</node>
+<node TEXT="D&#xe9;finition" ID="ID_859464541" CREATED="1537371026818" MODIFIED="1537372256498">
+<font SIZE="12"/>
+<node TEXT="UI Design" ID="ID_1123341820" CREATED="1537370862473" MODIFIED="1537372260856">
+<font SIZE="12"/>
+<node TEXT="Partie &quot;visible&quot;, &quot;perceptible&quot; d&apos;un produit : c&apos;est ce que l&apos;on voit, entend, touche" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_179418822" CREATED="1537370925551" MODIFIED="1537372267059">
+<font SIZE="12"/>
+</node>
+<node TEXT="Web Social Sensoriel" ID="ID_284111165" CREATED="1537370946077" MODIFIED="1537372287025">
+<font SIZE="12"/>
+</node>
+</node>
+<node TEXT="UX Design" ID="ID_1635064815" CREATED="1537371045088" MODIFIED="1537372263970">
+<font SIZE="12"/>
+<node TEXT="Parties visibles &amp; invisibles qui contribuent &#xe0; l&apos;exp&#xe9;rience globale de l&apos;utilisateur." LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_322804317" CREATED="1537370882679" MODIFIED="1537372279248">
+<font SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Notions" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" POSITION="right" ID="ID_1118780235" CREATED="1462729792536" MODIFIED="1537371202114" BACKGROUND_COLOR="#ffb2d4">
+<node TEXT="Usuabilit&#xe9;" ID="ID_531505635" CREATED="1537371119697" MODIFIED="1537372131343">
+<font SIZE="12"/>
+<node TEXT="Degr&#xe9; selon lequel un produit peut &#xea;tre utilis&#xe9;" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_783356593" CREATED="1537371369260" MODIFIED="1537372128519">
+<font SIZE="12"/>
+</node>
+<node TEXT="Permet aux utilisateurs d&apos;accomplir un ensemble de t&#xe2;ches avec simplicit&#xe9;." LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_146144729" CREATED="1537371417053" MODIFIED="1537372125047">
+<font SIZE="12"/>
+</node>
+</node>
+<node TEXT="Taux de Retention" ID="ID_331319012" CREATED="1537371534521" MODIFIED="1537372134459">
+<font SIZE="12"/>
+<node TEXT="D&#xe9;finition" ID="ID_897368170" CREATED="1537371733613" MODIFIED="1537372138946">
+<font SIZE="12"/>
+<node TEXT="Taux des utilisateurs qui continuent d&apos;aller sur une app ou un site web apr&#xe9;s plus de 3 mois apr&#xe9;s sa sorti" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_181466925" CREATED="1537371588634" MODIFIED="1537372113722">
+<font SIZE="12"/>
+</node>
+</node>
+<node TEXT="Accrocher les utilisateurs" ID="ID_31750686" CREATED="1537371754705" MODIFIED="1537372141891">
+<font SIZE="12"/>
+<node TEXT="&#xc9;l&#xe9;ment d&#xe9;clancheur :" ID="ID_108845173" CREATED="1537371795974" MODIFIED="1537372147397">
+<font SIZE="12"/>
+<node TEXT="Interne" ID="ID_612921918" CREATED="1537371885095" MODIFIED="1537372154832">
+<font SIZE="12"/>
+<node TEXT="Tire l&apos;utilisateur vers l&apos;application" ID="ID_874994642" CREATED="1537371891656" MODIFIED="1537372161132">
+<font SIZE="12"/>
+</node>
+<node TEXT=" &#xc9;tats psychologiques ou des &#xe9;motions" ID="ID_30262414" CREATED="1537371918191" MODIFIED="1537372164219">
+<font SIZE="12"/>
+</node>
+<node TEXT="Solitude, ennui, combler un besoin" ID="ID_775101951" CREATED="1537371934660" MODIFIED="1537372169674">
+<font SIZE="12"/>
+</node>
+</node>
+<node TEXT="Externe" ID="ID_775123849" CREATED="1537371961342" MODIFIED="1537372158071">
+<font SIZE="12"/>
+<node TEXT="Pousse l&apos;utilisateur vers l&apos;application" ID="ID_1072996173" CREATED="1537371988996" MODIFIED="1537372173322">
+<font SIZE="12"/>
+</node>
+</node>
+</node>
+<node TEXT="Action souhait&#xe9;e" ID="ID_1448029656" CREATED="1537372031628" MODIFIED="1537372151405">
+<font SIZE="12"/>
+<node TEXT="&#xc9;liminer les frictions qui freine les d&#xe9;marches de l&apos;utilisateur" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_403203500" CREATED="1537372057817" MODIFIED="1537372185512">
+<font SIZE="12"/>
+</node>
+<node TEXT="R&#xe9;duire les charges cognitives, rendre les actions simple" ID="ID_1849441447" CREATED="1537372194314" MODIFIED="1537372223479">
+<font SIZE="12"/>
+</node>
+</node>
+<node TEXT="R&#xe9;compense" ID="ID_1795346650" CREATED="1537372305477" MODIFIED="1537372313350">
+<font SIZE="12"/>
+<node TEXT="&#xc9;l&#xe9;ment de surprise" ID="ID_91651295" CREATED="1537372369035" MODIFIED="1537372409458">
+<font SIZE="12"/>
+<node TEXT="Toujours prolonger l&apos;exp&#xe9;rience pour une meilleur surprise" ID="ID_1290608002" CREATED="1537372412775" MODIFIED="1537372428922">
+<font SIZE="12"/>
+</node>
+</node>
+</node>
+<node TEXT="Investissement" ID="ID_1277362398" CREATED="1537372439126" MODIFIED="1537372447368">
+<font SIZE="12"/>
+<node TEXT="Data" ID="ID_1177965531" CREATED="1537372473958" MODIFIED="1537372496285">
+<font SIZE="12"/>
+</node>
+<node TEXT="Augmentation de chances de r&#xe9;compenses" ID="ID_1942080431" CREATED="1537372479453" MODIFIED="1537372492714">
+<font SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Design collaboratif" ID="ID_263808165" CREATED="1538575221236" MODIFIED="1538575231921">
+<font SIZE="12"/>
+<node TEXT="Travailler en groupe, utiliser les connaissances de chacun" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" FOLDED="true" ID="ID_1769671325" CREATED="1538575272047" MODIFIED="1538575300144">
+<font SIZE="12"/>
+<node TEXT="D&#xe9;veloppement en ligne d&apos;interfaces" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_1220627930" CREATED="1538575318908" MODIFIED="1538575342075">
+<font SIZE="13"/>
+</node>
+</node>
+<node TEXT="Utilisation des data pour voir les attentes utilisateurs" ID="ID_1798974110" CREATED="1538575849719" MODIFIED="1538575898862">
+<font SIZE="12"/>
+<node TEXT="Big-Data" ID="ID_28920694" CREATED="1538575901681" MODIFIED="1538575922183">
+<font SIZE="12"/>
+</node>
+<node TEXT="Small Data" ID="ID_1301329244" CREATED="1538575909516" MODIFIED="1538575918340">
+<font SIZE="12"/>
+</node>
+</node>
+</node>
+<node TEXT="Design d&apos;anticipation" ID="ID_1256193553" CREATED="1538575366854" MODIFIED="1538575415927">
+<font SIZE="12"/>
+<node TEXT="Pr&#xe9;voir les besoins de l&apos;utilisateurs" ID="ID_1368005831" CREATED="1538575379403" MODIFIED="1538575412079">
+<font SIZE="12"/>
+</node>
+</node>
+<node TEXT="Web Social Sensitif" ID="ID_615885678" CREATED="1538575484535" MODIFIED="1538575511408">
+<font SIZE="12"/>
+<node TEXT="Stimuler les 5 sens &#xe0; travers de miro-interactions" ID="ID_1092317704" CREATED="1538575584082" MODIFIED="1538575621598">
+<font SIZE="12"/>
+<node TEXT="Int&#xe9;grer les &#xe9;motions humaine" ID="ID_420692219" CREATED="1538575768262" MODIFIED="1538575789809">
+<font SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Dimension psychlogique" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" POSITION="left" ID="ID_1332007370" CREATED="1462729844988" MODIFIED="1537439686886" BACKGROUND_COLOR="#ffd5ad" VGAP_QUANTITY="5.0 pt">
+<node TEXT="Connaitre et comprendre les utilisateurs" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_1308316036" CREATED="1537434283482" MODIFIED="1537439781113">
+<font SIZE="12"/>
+<node TEXT="Besoin des utilisateurs et probl&#xe8;mes qu&apos;ils rencontrent dans la vie" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_851987941" CREATED="1537439372416" MODIFIED="1537439476840">
+<font SIZE="12"/>
+</node>
+<node TEXT="Ce centrer autour de l&apos;utilisateur" ID="ID_874407662" CREATED="1537439615982" MODIFIED="1537439632937">
+<font SIZE="12"/>
+</node>
+<node TEXT="Design = compr&#xe9;hension psychologie humaine" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_262756168" CREATED="1537434477730" MODIFIED="1537439785667">
+<font SIZE="12"/>
+</node>
+<node TEXT="Les utilisateurs chercheront des yeux l&apos;information it&#xe9;ressante, ils ne vont pas lire la page en entier" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_1073153619" CREATED="1537439898844" MODIFIED="1537439975166">
+<font SIZE="12"/>
+</node>
+</node>
+<node TEXT="&#xc9;tudes de march&#xe9; &amp; Test utilisateurs" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_1797290324" CREATED="1537439446681" MODIFIED="1537439789935">
+<font SIZE="12"/>
+</node>
+</node>
+<node TEXT="Mobile-first" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" POSITION="right" ID="ID_1983956508" CREATED="1462728817058" MODIFIED="1537439707782" BACKGROUND_COLOR="#cccee9">
+<node TEXT="Utilisateur est mobile, connect&#xe9; dans les transports" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" FOLDED="true" ID="ID_1843738772" CREATED="1537439711747" MODIFIED="1537439775481">
+<font SIZE="12"/>
+<node TEXT="Partie int&#xe9;grante de la vie de tout les jours" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_429924837" CREATED="1537439810116" MODIFIED="1537439826972">
+<font SIZE="12"/>
+</node>
+</node>
+<node TEXT="Traiter les attentes du mobile en premier (maquettage)" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_1452376922" CREATED="1537440147051" MODIFIED="1537440184857">
+<font SIZE="12"/>
+<node TEXT="Taille de l&apos;&#xe9;cran petite" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" FOLDED="true" ID="ID_649745106" CREATED="1537440211790" MODIFIED="1537440275414">
+<font SIZE="12"/>
+<node TEXT="Afficher l&apos;essentiel du contenu, taille - endroit" LOCALIZED_STYLE_REF="defaultstyle.floating" ID="ID_467538495" CREATED="1537440357572" MODIFIED="1537440395916">
+<font SIZE="12"/>
+</node>
+</node>
+<node TEXT="Utiliser dans les transport, en marchant etc" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_472863469" CREATED="1537440244030" MODIFIED="1537440279286">
+<font SIZE="12"/>
+<node TEXT="Pas consommer trop d&apos;&#xe9;nergie" LOCALIZED_STYLE_REF="defaultstyle.floating" ID="ID_1081004201" CREATED="1537440452523" MODIFIED="1537440472657">
+<font SIZE="12"/>
+</node>
+</node>
+<node TEXT="Design pour le pouce" ID="ID_596077300" CREATED="1537440097821" MODIFIED="1537440129894">
+<font SIZE="12"/>
+</node>
+<node TEXT="Al&#xe9;a du r&#xe9;seau" ID="ID_636377039" CREATED="1537440305496" MODIFIED="1537440324402">
+<font SIZE="12"/>
+<node TEXT="Charger rapidement les pages" LOCALIZED_STYLE_REF="defaultstyle.floating" ID="ID_45159736" CREATED="1537440417435" MODIFIED="1537440477568">
+<font SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Projets &amp; Comp&#xe9;tences" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" POSITION="left" ID="ID_1222821095" CREATED="1462729713743" MODIFIED="1537439697684" BACKGROUND_COLOR="#b8b597">
+<node TEXT="Projets PPE" ID="ID_459159961" CREATED="1538574033486" MODIFIED="1538575238854">
+<font SIZE="12"/>
+<node TEXT="Troc de temps" ID="ID_1515538156" CREATED="1538574207504" MODIFIED="1538575236497">
+<font SIZE="12"/>
+</node>
+</node>
+<node TEXT="Projets Perso" ID="ID_1500156698" CREATED="1538574105363" MODIFIED="1538575241634">
+<font SIZE="12"/>
+<node TEXT="Site web de test" ID="ID_360482322" CREATED="1538574117106" MODIFIED="1538575244724">
+<font SIZE="12"/>
+</node>
+</node>
+</node>
+<node TEXT="Tendances" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" POSITION="right" ID="ID_1375264560" CREATED="1462728879440" MODIFIED="1538576293198" BACKGROUND_COLOR="#d89bd8">
+<node TEXT="Pull to Refresh" ID="ID_1253312790" CREATED="1538576295843" MODIFIED="1538576306483">
+<font SIZE="12"/>
+</node>
+</node>
+</node>
+</map>
